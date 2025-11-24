b8a32a72ee9865a1dfd93eda118d96a45255acc5
Atulesh <atul2004v@gmail.com>
2025-11-24 20:28:02 +0530

    Updated Blynk_server

--- src/Blynk_server.cpp
+++ src/Blynk_server.cpp
@@ -3,10 +3,36 @@
 #define BLYNK_AUTH_TOKEN "SOMETHING_SOMETHING_AGAIN"
 
 #include <WiFi.h>
+#include "sensors.h"
+#include <sensors.h>
 #include <Arduino.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+int output_value;
+int moisturelevel;
+
+int notified = 0;
+int timedelay= 60000L;
+
+void Sensors(){
+    Blynk.virtualWrite(V0, soilMoisture);
+    Blynk.virtualWrite(V1, temperature);
+    Blynk.virtualWrite(V2, humidity);
+}
+
 char ssid[] = "your_ssid";
 char password[] = "your_password";
 
+BlynkTimer timer;
+
+void setup(){
+    Serial.begin(115200);
+    Blynk.begin( BLYNK_AUTH_TOKEN,ssid, password);
+    timer.setInterval(timedelay, Sensors);
+}
+
+void loop(){
+    Blynk.run();
+    timer.run();
+}
